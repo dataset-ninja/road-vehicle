6c84a3104cb317ca7fded2b687ca5bcc975432d9
almaz <almazgimaev@gmail.com>
2023-06-14 03:00:19 +0300

    add settings and convert

--- src/convert.py
+++ src/convert.py
@@ -1,15 +1,81 @@
-# Path to the original dataset
+# https://www.kaggle.com/datasets/ashfakyeafi/road-vehicle-images-dataset
+
+import os
 
 import supervisely as sly
+import yaml
+from supervisely.io.fs import get_file_name
+from tqdm import tqdm
 
 
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    # * dataset_path = "Path to the local dir with dataset"
+    dataset_path = "/Users/almaz/Downloads/trafic_data"
+    batch_size = 30
 
-    # Function should read local dataset and upload it to Supervisely project, then return project info.
+    def _create_ann(image_path):
+        labels = []
 
-    raise NotImplementedError("The converter should be implemented manually.")
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
 
-    # * return project
+        ann_path = os.path.join(annotations_pathes, get_file_name(image_path) + ".txt")
+
+        with open(ann_path) as f:
+            content = f.read().split("\n")
+
+        for curr_data in content:
+            if len(curr_data) != 0:
+                ann_data = list(map(float, curr_data.split(" ")))
+                curr_obj_class = idx_to_obj_class[int(ann_data[0])]
+                left = int((ann_data[1] - ann_data[3] / 2) * img_wight)
+                right = int((ann_data[1] + ann_data[3] / 2) * img_wight)
+                top = int((ann_data[2] - ann_data[4] / 2) * img_height)
+                bottom = int((ann_data[2] + ann_data[4] / 2) * img_height)
+                rectangle = sly.Rectangle(top=top, left=left, bottom=bottom, right=right)
+                label = sly.Label(rectangle, curr_obj_class)
+                labels.append(label)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
+
+    yaml_path = os.path.join(dataset_path, "data_1.yaml")
+    idx_to_obj_class = {}
+    with open(yaml_path, "r") as stream:
+        yaml_data = yaml.safe_load(stream)
+        for idx, class_name in enumerate(yaml_data["names"]):
+            idx_to_obj_class[idx] = obj_class = sly.ObjClass(class_name, sly.Rectangle)
+
+    obj_class_collection = sly.ObjClassCollection(list(idx_to_obj_class.values()))
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(obj_classes=obj_class_collection)
+    api.project.update_meta(project.id, meta.to_json())
+
+    data = os.listdir(dataset_path)
+    for ds_name in data:
+        curr_item_path = os.path.join(dataset_path, ds_name)
+        if os.path.isdir(curr_item_path):
+            dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+            images_pathes = os.path.join(curr_item_path, "images")
+            annotations_pathes = os.path.join(curr_item_path, "labels")
+            images_names = os.listdir(images_pathes)
+
+            progress = tqdm(total=len(images_names), desc="Create dataset {}".format(ds_name))
+
+            for img_names_batch in sly.batched(images_names, batch_size=batch_size):
+                images_pathes_batch = [
+                    os.path.join(images_pathes, image_path) for image_path in img_names_batch
+                ]
+
+                anns_batch = [_create_ann(image_path) for image_path in images_pathes_batch]
+
+                img_infos = api.image.upload_paths(dataset.id, img_names_batch, images_pathes_batch)
+                img_ids = [im_info.id for im_info in img_infos]
+
+                api.annotation.upload_anns(img_ids, anns_batch)
+
+                progress.update(len(img_names_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -5,31 +5,33 @@ from dataset_tools.templates import AnnotationType, CVTask, Industry, License
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: Optional[str] = None
+PROJECT_NAME: str = "Road Vehicle"
+PROJECT_NAME_FULL: Optional[str] = "Road Vehicle Images Dataset"
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-INDUSTRIES: List[Industry] = None
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+LICENSE: License = License.DbCL_1_0()
+INDUSTRIES: List[Industry] = [Industry.Industrial()]
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
-RELEASE_YEAR: int = None
-HOMEPAGE_URL: str = None
+RELEASE_YEAR: int = 2021
+HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/ashfakyeafi/road-vehicle-images-dataset"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 387318
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/road-vehicle"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[
+    Union[str, dict]
+] = "https://www.kaggle.com/datasets/ashfakyeafi/road-vehicle-images-dataset/download?datasetVersionNumber=1"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -47,7 +49,7 @@ TAGS: List[str] = None
 
 
 def check_names():
-    fields_before_upload = [PROJECT_NAME] # PROJECT_NAME_FULL
+    fields_before_upload = [PROJECT_NAME]  # PROJECT_NAME_FULL
     if any([field is None for field in fields_before_upload]):
         raise ValueError("Please fill all fields in settings.py before uploading to instance.")
 
@@ -64,10 +66,10 @@ def get_settings():
         "preview_image_id": PREVIEW_IMAGE_ID,
         "github_url": GITHUB_URL,
     }
-    
+
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
-   
+
     settings["project_name_full"] = PROJECT_NAME_FULL
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
