837b6e423a82a2540a7270f3415fe543e8fcf409
Almaz <79905215+almazgimaev@users.noreply.github.com>
2023-06-27 12:23:52 +0300

    Update options.py

--- src/options.py
+++ src/options.py
@@ -34,7 +34,7 @@ VERTICAL_GRID_IS_DETECTION_TASK: bool = IS_DETECTION_TASK
 ###############################################################
 # * Set up visualization params for SideAnnotationsGrid class #
 SIDE_ANNOTATIONS_GRID_ROWS: int = 2
-SIDE_ANNOTATIONS_GRID_COLS: int = 3
+SIDE_ANNOTATIONS_GRID_COLS: int = 4
 SIDE_ANNOTATIONS_GRID_IS_DETECTION_TASK: bool = IS_DETECTION_TASK
 ###############################################################
 
